e2a7d7568910f3f9765bd9b2f471cc8c07eae40f
Sonja Johnson-Yu <sonja@Sonjas-MacBook-Pro-3.local>
2022-03-07 22:10:42 -0500

    Fix one outstanding merge conflict

--- dfl/utils.py
+++ dfl/utils.py
@@ -254,22 +254,11 @@ class DiffTopK(object):
 
         return tf.stop_gradient(Gamma), gradient_function
 
-<<<<<<< HEAD
-def getSoftTopk(a, k):
-    if len(a.shape) == 2:
-        bs, n = a.shape
-    else:
-        bs, n, _ = a.shape
-    diffTopK = DiffTopK(k=k)
-    gamma = diffTopK(-a)
-    probs = gamma[:,:,0].numpy() * n / k
-=======
 def getSoftTopk(a, k, epsilon=0.1):
     bs, n = a.shape
     diffTopK = DiffTopK(k=k, epsilon=epsilon)
     gamma = diffTopK(-a).numpy()
     probs = gamma[:,:,0] / np.sum(gamma[:,:,0], axis=1, keepdims=True)
->>>>>>> dfl
 
     selection = []
     for i in range(bs):
